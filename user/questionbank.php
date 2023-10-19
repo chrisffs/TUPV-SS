6a5ec14ddd5842ae6b0a5a6036d9f8eb388a3ee5
Rodolfo Baylen <baylenr997@gmail.com>
2023-10-19 19:23:04 +0800

    some changes on user

---
+++ user/questionbank.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/css/main.css">
+    <link rel="icon" href="../src/img/tupvlogo.png">
+    <title>Question Bank | TUPV Syllabus System</title>
+</head>
+<body>
+<?php 
+$page = 'questionbank';
+include "../php/user_header.php";
+?>
+
+<script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script>
+// $(document).ready(function() {
+//   // Hide the button initially
+//   var modalButton = $('[data-modal-toggle="extralarge-modal"]');
+//   modalButton.hide();
+
+//   // Use setTimeout to trigger a click event after 2 seconds
+//   setTimeout(function() {
+//     modalButton.click();
+//   }, 1300); // 2000 milliseconds = 2 seconds
+// });
+</script>
+</body>
+</html>
