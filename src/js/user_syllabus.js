44de07851c636c4c77d89135a6b2d411185ce1fa
Christian Noel Salvador <christiannoelsalvador24@gmail.com>
2023-10-20 18:23:56 +0800

    some changes

---
+++ src/js/user_syllabus.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // Hide the button initially
+    var modalButton = $('[data-modal-toggle="extralarge-modal"]');
+    modalButton.hide();
+  
+    // Use setTimeout to trigger a click event after 2 seconds
+    setTimeout(function() {
+      modalButton.click();
+    }, 1300);
+
+    var filter = $('#filter');
+    var menuContainer = $('#folders');
+
+    filter.on('keyup', searchMenu);
+
+    function searchMenu(e) {
+        var typedText = e.target.value.toLowerCase();
+        var menuItems = menuContainer.find('a');
+        
+        menuItems.each(function() {
+            var menuItem = $(this);
+            var menuNames = menuItem.find('h1, h2');
+            var showMenuItem = false;
+
+            menuNames.each(function() {
+                var menuName = $(this).text().toLowerCase();
+                if (menuName.indexOf(typedText) !== -1) {
+                    showMenuItem = true;
+                }
+            })
+            if (showMenuItem) {
+                menuItem.addClass('block');
+                menuItem.removeClass('hidden');
+            } else {
+                menuItem.addClass('hidden');
+            }
+        })
+    }
+});
