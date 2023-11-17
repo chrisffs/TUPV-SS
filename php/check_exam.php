ce7b09a766af28c695ba0e0bf68320923141a3e5
John Mark Delima <delimajohnmark14@gmail.com>
2023-11-17 23:43:34 +0800

    Test script using temporary data

--- php/check_exam.php
+++ php/check_exam.php
@@ -1,10 +1,26 @@
 <?php
 
-// Upload Image
+// Upload the image 
 
-// Get the exam data: answerkeys, options, items
+// Connect to the database to get the answer key and bubble options, use the exam code
 
-// $output = shell_exec('python omrscanner.py --image <image> --options <4> --items <20>');
-// echo $output;
+// Temporary Data
+$bubble_options = 4;
+$image_path = "../orm/test.png"; // Replace with the actual image path
+$answer_key = "3 3 3 3 3 2 1 3 2 3 3 2 3 3 2 2 2 4 1 1";
+$image_name = "img-test2"; // Change with name or code
 
-?> 
+$python_script = "python ../orm/omrscanner.py \"$image_path\" \"$answer_key\" $bubble_options $image_name";
+
+// Execute the Python script and capture the output
+$output = shell_exec($python_script);
+
+$result = json_decode($output, true);
+
+echo "Result: \n";
+print_r($result);
+
+// Update the database
+// Use the result
+// header('location: ../user/examchecker.php');
+?>
